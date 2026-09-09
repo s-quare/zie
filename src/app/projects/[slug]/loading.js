b368bd9395193852bfc4e37c3cc4a5a7edfa5ea0
s-quare <okeyprosperebubealex14@gmail.com>
2026-09-09 02:21:49 +0100

    things

--- src/app/projects/[slug]/loading.js
+++ src/app/projects/[slug]/loading.js
@@ -2,7 +2,7 @@ import { Loader2 } from "lucide-react";
 
 export default function Loading() {
   return (
-    <div className="fixed inset-0 z-9999 h-dvh w-full bg-black flex items-center justify-center">
+    <div className="fixed inset-0 z-9999 h-dvh w-full bg-coffee-dark flex items-center justify-center">
       <Loader2 className="w-8 h-8 text-coffee-tan animate-spin" />
     </div>
   );
